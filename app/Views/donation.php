70e3fa35596053dacd70ed8092295477e7a841e3
likevishal <ivishalonline@gmail.com>
2022-02-11 20:26:53 +0530

    Add donation controller and view page

---
+++ app/Views/donation.php
@@ -0,0 +1,85 @@
+<div role="main" class="main">
+    <section class="page-header page-header-classic page-header-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+                    <h1 data-title-border>Donate now</h1>
+                </div>
+                <div class="col-md-4 order-1 order-md-2 align-self-center">
+                    <ul class="breadcrumb d-block text-md-right">
+                        <li><a href="#">Home</a></li>
+                        <li class="active">Donate now</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--
+    <div id="googlemaps" class="google-map mt-0" style="height: 500px;"></div>-->
+
+                <div class="container">
+
+                    <div class="row py-4">
+                        <div class="col-lg-6">
+
+                            <h2 class="font-weight-bold text-8 mt-2 mb-0">Donate Now</h2>                            
+
+                            <form class="contact-form" action="#" method="POST">
+                                <div class="contact-form-success alert alert-success d-none mt-4">
+                                    <strong>Success!</strong> Your message has been sent to us.
+                                </div>
+
+                                <div class="contact-form-error alert alert-danger d-none mt-4">
+                                    <strong>Error!</strong> There was an error sending your message.
+                                    <span class="mail-error-message text-1 d-block"></span>
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-lg-4">
+                                        <label class="mb-1 text-2">First Name</label>
+                                        <input type="text" value="" data-msg-required="Please enter your first name." maxlength="100" class="form-control text-3 h-auto py-2" name="firstname" required>
+                                    </div>
+
+                                    <div class="form-group col-lg-4">
+                                        <label class="mb-1 text-2">Middle Name</label>
+                                        <input type="text" value="" data-msg-required="Please enter your middle name." maxlength="100" class="form-control text-3 h-auto py-2" name="middlename" required>
+                                    </div>
+
+                                    <div class="form-group col-lg-4">
+                                        <label class="mb-1 text-2">Last Name</label>
+                                        <input type="text" value="" data-msg-required="Please enter your last name." maxlength="100" class="form-control text-3 h-auto py-2" name="lastname" required>
+                                    </div>                                    
+                                </div>
+                                <div class="form-row">                               
+                                    <div class="form-group col">
+                                        <label class="mb-1 text-2">Email Address</label>
+                                        <input type="email" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control text-3 h-auto py-2" name="email" required>
+                                    </div>
+                                </div>
+                                <div class="form-row">                               
+                                    <div class="form-group col">
+                                        <label class="mb-1 text-2">Mobile Number</label>
+                                        <input type="number" value="" data-msg-required="Please enter mobile number." data-msg-email="Please enter a valid email address." maxlength="10" class="form-control text-3 h-auto py-2" name="mobile" required>
+                                    </div>
+                                </div>                                
+                                <div class="form-row">
+                                    <div class="form-group col">
+                                        <label class="mb-1 text-2">Comment</label>
+                                        <textarea maxlength="5000" data-msg-required="Please enter your comment." rows="8" class="form-control text-3 h-auto py-2" name="message" ></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col">
+                                        <input type="submit" value="Donate Now" class="btn btn-primary btn-modern" data-loading-text="Loading...">
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div class="col-lg-6">
+
+                        </div>
+
+                    </div>
+
+                </div>
