2e14fc74e692466dc19062db66e81c3091e6b6a1
likevishal <ivishalonline@gmail.com>
2022-02-15 20:01:12 +0530

    All the code for store and update payments data to database

--- app/Controllers/Donation.php
+++ app/Controllers/Donation.php
@@ -2,29 +2,33 @@
 require(APPPATH.'/ThirdParty/razorpay-php/Razorpay.php');
 use Razorpay\Api\Api;
 use Razorpay\Api\Errors\SignatureVerificationError; 
+use App\Models\Donation_model;
 use App\Models\Customers_model;
 class Donation extends BaseController
 {
     private $security;     
     protected $session;
     private $Customers_m;
-    public function __construct() {         
+    private $Donation_m;
+    public function __construct() {                
         $this->session = \Config\Services::session();
         $this->session->start(); 
         helper('url');
         helper('functions');
         $this->Customers_m = new Customers_model(); 
+        $this->Donation_m = new Donation_model(); 
         $this->security = \Config\Services::security();          
                          
     }
     public function donation()
     {
+    
         helper('form');
         $data['title'] = DONATE_NOW; 
         echo front_view('donation',$data);
     }
 
-    public function donation_step_2(){  
+    public function donation_step_2(){        
         helper('form');      
         $fullName = (isset($_REQUEST['fullname'])) ? trim($_REQUEST['fullname']) : "";
         $emailAddress = (isset($_REQUEST['email'])) ? trim($_REQUEST['email']) : "";
@@ -55,7 +59,7 @@ class Donation extends BaseController
 
             $create_order = $api->order->create(
                                             array(
-                                                'receipt' => '123',
+                                                'receipt' => '',
                                                 'amount' => $amount * 100,
                                                 'currency' => 'INR',
                                                 'notes'=>
@@ -70,27 +74,71 @@ class Donation extends BaseController
             $razer_orders_data = $array[$prefix.'attributes'];
             $razer_orders_id = $razer_orders_data['id'];
 
-          
-            $paymentsData = array(
-                'fullName' => $fullName,
-                'emailAddress' => $emailAddress,
-                'mobileNumber' => $mobileNumber,
-                'amount' => $amount,
-                'orders_id' => $razer_orders_id
-            );
-            $data['razerPay'] = $paymentsData;        
-            $data['title'] = DONATE_NOW_STEP_2; 
-            echo front_view('donation_step_2',$data);
+            if($razer_orders_id){
+                $date = date("Y-m-d H:i:s");
+                $ordersData = array(
+                    'customers_id' => $customers_id,
+                    'razer_orders_id' => $razer_orders_id,
+                    'order_creation_date' => $date
+                );
+                $child_oreders_id = $this->Donation_m->create_order($ordersData);
+                if($child_oreders_id){
+                    $paymentsData = array(
+                        'fullName' => $fullName,
+                        'emailAddress' => $emailAddress,
+                        'mobileNumber' => $mobileNumber,
+                        'amount' => $amount,
+                        'orders_id' => $razer_orders_id
+                    );
+                    $data['razerPay'] = $paymentsData;        
+                    $data['title'] = DONATE_NOW_STEP_2; 
+                    echo front_view('donation_step_2',$data);
+                }               
+            }
         }
        
     }
 
     public function donation_success(){         
         $api = new Api(RAZERPAY_KEY,RAZERPAY_KEY_SECRET);
-        $res=$api->payment->fetch($_REQUEST['razorpay_payment_id']);
-        print_r($res);die;
-        $data['title'] = DONATE_SUCCESS; 
-        echo front_view('donation_success',$data);
+        $res = $api->payment->fetch($_REQUEST['razorpay_payment_id']);
+
+        $res = (array) $res;
+        $prefix = chr(0).'*'.chr(0);
+        $razer_payments_data = $res[$prefix.'attributes'];
+
+        if((is_array($razer_payments_data) && sizeof($razer_payments_data) > 0 ) &&
+            (isset($razer_payments_data['order_id']) && $razer_payments_data['order_id'] != '') &&
+            (isset($razer_payments_data['status']) && $razer_payments_data['status'] == 'captured') &&
+            (isset($razer_payments_data['captured']) && ($razer_payments_data['captured'] == 1 || $razer_payments_data['captured'] == '1' ))
+        ){
+            $rp = $razer_payments_data;
+            $orders_id = $razer_payments_data['order_id'];
+            $paymentDate = date("Y-m-d H:i:s");
+            $payments = array(
+                'razer_payment_id' => $rp['id'],
+                'currency' => $rp['currency'],
+                'amount' => $rp['amount']/100,
+                'status' => $rp['status'],
+                'captured' =>  $rp['captured'],
+                'card_id' =>  $rp['card_id'],
+                'method' => $rp['method'],
+                'amount_refunded' => $rp['amount_refunded'],
+                'refund_status' => $rp['refund_status'],
+                'bank' => $rp['bank'],
+                'wallet' => $rp['wallet'],
+                'vpa' => $rp['vpa'],
+                'international' => $rp['international'],
+                'fee' => $rp['fee'],
+                'tax' => $rp['tax'],
+                'entity' => $rp['entity'],
+                'payement_date' => $paymentDate,
+            );
+
+            $update_payments = $this->Donation_m->update_donation($orders_id,$payments);
+            $data['title'] = DONATE_SUCCESS; 
+            echo front_view('donation_success',$data);
+        }        
     }
     public function page404() {        
         $data['title'] = 'error';        
---
+++ app/Models/Donation_model.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+
+class Donation_model extends Model {
+    
+    protected $db;
+    protected $session;   
+    public function __construct() {
+        $this->session = session();
+        $this->db = db_connect();
+        helper('functions');
+    }  
+    
+    public function create_order($orders_data){
+        $orders = $this->db->table('child_wish_donation');
+        $orders->insert($orders_data);
+        return $this->db->insertID();
+    }
+
+    public function update_donation($razer_orders_id,$donationData){
+        $builder = $this->db->table('child_wish_donation');
+        $builder->where('razer_orders_id', $razer_orders_id);        
+        return $builder->update($donationData);
+    }
+   
+}
