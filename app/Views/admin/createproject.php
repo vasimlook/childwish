40b827a21712802006ce5f1bf7216278df43c486
likevishal <ivishalonline@gmail.com>
2022-03-04 15:59:38 +0530

    Add view for the create projects on the backend

--- app/Views/admin/createproject.php
+++ app/Views/admin/createproject.php
@@ -19,13 +19,13 @@
                                 <div class="row g-3 align-center">
                                     <div class="col-lg-4">
                                         <div class="form-group">
-                                            <label class="form-label float-right" for="user_current_password">Current Password:</label>                                            
+                                            <label class="form-label float-right" for="projects_title">Projects Title:</label>                                            
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="form-group">
                                             <div class="form-control-wrap">
-                                                <input type="password" class="form-control" name="user_current_password" id="user_current_password" placeholder="Enter Current Password" required="" autocomplete="off">
+                                                <input type="text" class="form-control" name="projects_title" id="projects_title" placeholder="Enter projects title" required="" autocomplete="off">
                                             </div>
                                         </div>
                                     </div>
@@ -33,13 +33,13 @@
                                 <div class="row g-3 align-center">
                                     <div class="col-lg-4">
                                         <div class="form-group">                                            
-                                            <label class="form-label float-right" for="user_new_password">New Password:</label>
+                                            <label class="form-label float-right" for="projects_description">Projects Descriptions:</label>
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="form-group">
                                             <div class="form-control-wrap">
-                                                <input type="password" class="form-control" name="user_new_password" id="user_new_password" placeholder="Enter New Password" required="" autocomplete="off">
+                                                <textarea class="form-control" name="projects_description" id="projects_description" required></textarea>
                                             </div>
                                         </div>
                                     </div>
@@ -47,13 +47,69 @@
                                 <div class="row g-3 align-center">
                                     <div class="col-lg-4">
                                         <div class="form-group">                                             
-                                             <label class="form-label float-right" for="user_confirm_password">Confirm Password: </label>
+                                             <label class="form-label float-right" for="projects_image">Projects Image: </label>
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="form-group">
                                             <div class="form-control-wrap">
-                                                 <input type="password" class="form-control" name="user_confirm_password" id="user_confirm_password" placeholder="Enter Confirm Password" required autocomplete="off">
+                                                 <input type="file"  name="projects_image" id="projects_image"  required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-3 align-center">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">                                             
+                                             <label class="form-label float-right" for="target_amount">Target Amount: </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <div class="form-control-wrap">
+                                                 <input type="text" class="form-control"  name="target_amount" id="target_amount" placeholder="Enter target amount" required="" autocomplete="off">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-3 align-center">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">                                             
+                                             <label class="form-label float-right" for="amount_start_date">Start Date: </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <div class="form-control-wrap">
+                                                 <input type="text" class="form-control"  name="amount_start_date" id="amount_start_date" placeholder="Enter start date" required="" autocomplete="off">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-3 align-center">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">                                             
+                                             <label class="form-label float-right" for="amount_end_date">End Date: </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <div class="form-control-wrap">
+                                                 <input type="text" class="form-control"  name="amount_end_date" id="amount_end_date" placeholder="Enter end date" required="" autocomplete="off">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-3 align-center">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">                                             
+                                             <label class="form-label float-right" for="urgent_need">Urgent need?: </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <div class="form-control-wrap">
+                                                 <input type="checkbox"  name="urgent_need" id="urgent_need" >
                                             </div>
                                         </div>
                                     </div>
@@ -62,7 +118,7 @@
                                 <div class="row g-3">
                                     <div class="col-lg-7 offset-lg-5">
                                         <div class="form-group mt-2">
-                                            <button type="submit" class="btn btn-lg btn-primary">Update</button>                                          
+                                            <button type="submit" class="btn btn-lg btn-primary">Create Project</button>                                          
                                         </div>
                                     </div>
                                 </div>
